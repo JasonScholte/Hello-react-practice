188cf35b52aad2b745cb2363314d5d3c703d82c3
JasonScholte <56888977+JasonScholte@users.noreply.github.com>
2020-08-26 01:39:16 -0500

    here is what I have so far.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+         To Dos App
+        </h1>
       </header>
+    </div>
+
+    <div className="Nav">
+      <ul>
+        <li>New List</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+
+    <div className="Lists">
+      <ol>
+      <li>blah</li>
+      <li>blah blah</li>
+      <li>blah blah blah</li>
+      </ol>
     </div>
   );
 }
